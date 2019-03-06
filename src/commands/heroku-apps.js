a58cd2549871b181f67f7de6fa134a697a44892e
Dan Smith <ds@bluetel.co.uk>
2019-03-06 16:36:32 +0000

    Add comments to apps / pipelines commands

--- src/commands/heroku-apps.js
+++ src/commands/heroku-apps.js
@@ -2,6 +2,10 @@ const {Command} = require('@oclif/command')
 const HerokuClient = require('../heroku-client')
 
 class HerokuAppsCommand extends Command {
+  /**
+   * Return a list of available apps via the Heroku API, useful for obtaining unique ids for further interactions
+   * @returns {Promise<void>}
+   */
   async run() {
     this.heroku = new HerokuClient({ token: process.env.HEROKU_API_TOKEN })
     const apps = await this.heroku.listApps()
--- src/commands/heroku-pipelines.js
+++ src/commands/heroku-pipelines.js
@@ -2,6 +2,10 @@ const { Command } = require('@oclif/command')
 const HerokuClient = require('../heroku-client')
 
 class HerokuPipelinesCommand extends Command {
+  /**
+   * Return a list of available pipelines via the Heroku API, useful for obtaining unique ids for further interactions
+   * @returns {Promise<void>}
+   */
   async run() {
     this.heroku = new HerokuClient({ token: process.env.HEROKU_API_TOKEN })
     const pipelines = this.heroku.listPipelines()
